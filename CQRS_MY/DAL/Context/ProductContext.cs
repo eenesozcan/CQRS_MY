f9fd2442be0e013b87d210c002b84670e9c80cac
Enes OZCAN <enesozcan@hotmail.com>
2022-12-12 22:28:53 +0300

    Add project files.

---
+++ CQRS_MY/DAL/Context/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using CQRS_MY.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS_MY.DAL.Context
+{
+    public class ProductContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-07T8MF2\\DESKTOP-CSTSJL1\\MSSQLSERVER2019;initial catalog=ZCQRSDb;integrated security=true");
+        }
+
+
+
+        public DbSet<Product> Products { get; set; }
+
+        
+    }
+}
